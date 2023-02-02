8f433a82400a9442595ddb87dfd6464ffd39e7b4
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-02-02 18:09:58 +0300

    correct mapper class, now it works with abstract superclasses as argument and return type

--- src/main/java/ru/ivashkevich/manhole_constructor/mapper/ManholeToEntityMapper.java
+++ src/main/java/ru/ivashkevich/manhole_constructor/mapper/ManholeToEntityMapper.java
@@ -1,12 +1,21 @@
 package ru.ivashkevich.manhole_constructor.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.SubclassExhaustiveStrategy;
+import org.mapstruct.SubclassMapping;
 import ru.ivashkevich.manhole_constructor.dao.ManholeEntity;
+import ru.ivashkevich.manhole_constructor.dao.SewerManholeEntity;
 import ru.ivashkevich.manhole_constructor.model.Manhole;
+import ru.ivashkevich.manhole_constructor.model.SewerManhole;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", subclassExhaustiveStrategy = SubclassExhaustiveStrategy.RUNTIME_EXCEPTION)
 public interface ManholeToEntityMapper {
-    ManholeEntity manholeToManholeEntity(Manhole manhole);
+    @SubclassMapping(target = SewerManholeEntity.class, source = SewerManhole.class)
+    ManholeEntity manholeToEntity(Manhole manhole);
 
-    Manhole manholeEntityToManhole(ManholeEntity entity);
+    @SubclassMapping(target = SewerManhole.class, source = SewerManholeEntity.class)
+    Manhole entityToManhole(ManholeEntity entity);
+
+    SewerManholeEntity sewerManholeToEntity(SewerManhole manhole);
+    SewerManhole entityToSewerManhole(SewerManholeEntity entity);
 }
