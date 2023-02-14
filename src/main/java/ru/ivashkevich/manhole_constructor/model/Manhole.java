d465e96c523cf8d656d6ab0c791ded1ca5f259d1
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-02-14 17:53:27 +0300

    change Manhole class

--- src/main/java/ru/ivashkevich/manhole_constructor/model/Manhole.java
+++ src/main/java/ru/ivashkevich/manhole_constructor/model/Manhole.java
@@ -1,17 +1,45 @@
 package ru.ivashkevich.manhole_constructor.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.EnumMap;
 
+@Getter
 public abstract class Manhole {
-    private static double minWorkChamberHeight;
 
     private String number;
-    private double trayDepth;
+    private double outletTrayDepth;
     private PavementType pavementType;
     private double manholeDiameter;
     private double pipeDiameter;
 
-    private EnumMap<ReinforcedConcreteElement, Integer> elements;
+    private double heightUnderPipe;
+    private double minHeight;
+    private double height;
 
+    private EnumMap<ReinforcedConcreteElement, Integer> neckElements;
 
+    private EnumMap<ReinforcedConcreteElement, Integer> workChamberElements;
+
+    public Manhole(String number, double outletTrayDepth, PavementType pavementType, double manholeDiameter, double pipeDiameter) {
+        this.number = number;
+        this.outletTrayDepth = outletTrayDepth;
+        this.pavementType = pavementType;
+        this.manholeDiameter = manholeDiameter;
+        this.pipeDiameter = pipeDiameter;
+
+        setHeightUnderPipe();
+        setMinHeight();
+    }
+
+    private void setMinHeight(){
+        minHeight = pavementType.getAdditionalHeight() + outletTrayDepth + heightUnderPipe;
+    }
+
+    protected abstract void setHeightUnderPipe();
+
+    protected abstract void constructManholeNeck();
+
+    protected abstract void constructManholeWorkChamber();
 }
