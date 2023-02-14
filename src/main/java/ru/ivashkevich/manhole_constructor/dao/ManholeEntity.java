0495aba57a8b97b29a4e6fed004685430b609f74
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-02-14 17:52:10 +0300

    fix abstract entity superclass annotation

--- src/main/java/ru/ivashkevich/manhole_constructor/dao/ManholeEntity.java
+++ src/main/java/ru/ivashkevich/manhole_constructor/dao/ManholeEntity.java
@@ -1,29 +1,40 @@
 package ru.ivashkevich.manhole_constructor.dao;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import org.hibernate.Hibernate;
 import ru.ivashkevich.manhole_constructor.model.PavementType;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "manholes")
+@MappedSuperclass       // вероятно такой вариант не подходит в случае добавления полей с EnumMap
 public abstract class ManholeEntity {
     private static double minWorkChamberHeight;
 
-    @Id // может ввести отдельное значение для ID? а номер сделать генерируемым, исходя из типа колодца?
+    @Id
     private String number;
-    @Column(name = "tray_depth")
-    private double trayDepth;
+    private double outletTrayDepth;
     private PavementType pavementType;
-    @Column(name = "manhole_diameter")
     private double manholeDiameter;
-    @Column(name = "pipe_diameter")
     private double pipeDiameter;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ManholeEntity that = (ManholeEntity) o;
+        return number != null && Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
